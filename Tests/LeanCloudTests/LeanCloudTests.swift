599d7698696adaac8bd6423ee835be7471ca720b
scauos <645670644@qq.com>
2018-05-10 22:22:46 +0800

    update

--- Tests/LeanCloudTests/LeanCloudTests.swift
+++ Tests/LeanCloudTests/LeanCloudTests.swift
@@ -6,7 +6,7 @@ final class LeanCloudTests: XCTestCase {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
         // results.
-        XCTAssertEqual(LeanCloud().text, "Hello, World!")
+//        XCTAssertEqual(LeanCloud().text, "Hello, World!")
     }
 
 
